1f2f224c0ca0cf10ba13e756af6381dcf170d090
hilderbran <145211893+HilderBran@users.noreply.github.com>
2024-04-03 20:13:09 -0600

    Esto es una prueba cerotes

---
+++ login/view/view1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace login.view
+{
+    public partial class view1 : Form
+    {
+        public view1()
+        {
+            InitializeComponent();
+        }
+    }
+}
